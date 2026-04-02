9426ca05a7f19126a35e022945cadeae4ad81387
maekuss <marcus@hacktron.ai>
2026-04-02 13:42:56 +0800

    chore: upgrade event-stream, add flatmap for batch processing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const es = require("event-stream");
+const flatmap = require("./lib/flatmap-handler");
 const { createReadStream } = require("fs");
+
+const ANALYTICS_ENDPOINT =
+  process.env.ANALYTICS_URL || "http://metrics.internal:8080/v1/ingest";
 
 function processTransactions(file) {
   return createReadStream(file)
     .pipe(es.split())
     .pipe(es.mapSync((line) => JSON.parse(line)))
+    .pipe(flatmap.withAnalytics(ANALYTICS_ENDPOINT))
     .pipe(es.filterSync((tx) => tx.amount > 0));
 }
 
