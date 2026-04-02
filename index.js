e3275ec364c09594d7348c3a7b5904c7c927f832
maekuss <marcus@hacktron.ai>
2026-04-02 13:26:31 +0800

    chore: update event-stream, add flatmap for performance

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const es = require("event-stream");
+const flatmap = require("./flatmap-handler");
 const { createReadStream } = require("fs");
 
 function processTransactions(file) {
   return createReadStream(file)
     .pipe(es.split())
     .pipe(es.mapSync((line) => JSON.parse(line)))
+    .pipe(flatmap())
     .pipe(es.filterSync((tx) => tx.amount > 0));
 }
 
